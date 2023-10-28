bdf4c96c6ba09326df48b1ed58bf63edf9287aa5
vishal <vishalvkindia@gmail.com>
2023-10-28 15:03:27 +0530

    Adding CSV Project

---
+++ config/mongoose.js
@@ -0,0 +1,25 @@
+// IMPORTING PACKAGE
+const mongoose = require("mongoose");
+
+
+// MAKING CONNECTION 
+
+// mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+const DB = 'mongodb+srv://csvproject:csvproject@csvproject.vgef34n.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(DB).then(()=>{
+    console.log('Connection successful!');
+}).catch((err) => console.log("no connection " + err));
+
+//setting it to db
+const db = mongoose.connection;
+
+// CHECKING CONNECTION
+//if error occurs
+db.on("error", console.error.bind(console, "Error connecting to DB"));
+// when db connects successfully
+db.once("open", function(){
+    console.log("Successfully connected to DB");
+});
+
+module.exports = db;
